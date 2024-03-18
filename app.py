0f7b68e57e4c0dca25e6f4edd994d1f7be59cc82
nii-armah-16 <161141812+nii-armah-16@users.noreply.github.com>
2024-03-18 03:49:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -124,6 +124,7 @@ def main():
                      with st.spinner('Wait for it...'):
                         time.sleep(1.5)                        
                         st.success('# Hello {} {} you qualify for a loan'.format(fname,lname), icon="✅")
+                        st.text(f'This prediction is based on a machine learning model with 83% accuracy score')
 
                 elif result[0] == 0:
                      with st.spinner('Wait for it...'):
